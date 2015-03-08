8bc3f01a440f092440f066a736aa7f646199006f
Aaron Krill <aaron@krillr.com>
2015-03-07 19:42:08 -0800

    Lol, colorama.

--- schmetterling.py
+++ schmetterling.py
@@ -1,3 +1,4 @@
+from colorama import init, Fore, Back, Style
 import audio, argparse, json, os, select, sys
 
 class Schmetterling:
@@ -15,43 +16,44 @@ class Schmetterling:
         missing = [x for x, y in self.options.items() if y == None]
         for x in missing:
             self.options[x] = self.interactive_config(x)
+            print Style.RESET_ALL
 
     def healthcheck(self):
         devices = self.audio.get_devices()
         if not [x for x in self.audio.get_devices() if x['maxInputChannels'] > 0]:
-            print "You do not appear to have any recording audio devices. Exiting."
+            print Back.RED + "You do not appear to have any recording audio devices. Exiting." + Style.RESET_ALL
             sys.exit(1)
         if not [x for x in self.audio.get_devices() if x['maxOutputChannels'] > 0]:
-            print "You do not appear to have any playback audio devices. Exiting."
+            print Back.RED + "You do not appear to have any playback audio devices. Exiting." + Style.RESET_ALL
             sys.exit(1)
 
     def interactive_config(self, option):
         if option == 'interval':
             options = ('odd', 'even')
-            choice = raw_input('Please choose an interval (odd/even): ').lower()
+            choice = raw_input(Style.BRIGHT + 'Please choose an interval (' + Fore.GREEN + 'odd' + Fore.RESET + '/' + Fore.GREEN + 'even' + Fore.RESET +'): ').lower()
             if not choice in options:
-                print 'Invalid choice'
-                return interactive_config(option)
+                print Fore.RED + 'Invalid choice' + Style.RESET_ALL
+                return self.interactive_config(option)
             return choice
         if option == 'recording_device':
             devices = [x for x in self.audio.get_devices() if x['maxInputChannels'] > 0]
-            print "Please choose a device to monitor for transmissions:"
+            print Style.BRIGHT + "Please choose a device to monitor for transmissions:"
             for x in range(len(devices)):
-                print "%i) %s" % (x, devices[x]['name'])
-            choice = raw_input('Device: ')
+                print (Style.BRIGHT + Fore.GREEN + "%i)" + Fore.BLUE + " %s" + Style.RESET_ALL) % (x, devices[x]['name'])
+            choice = raw_input(Style.BRIGHT + 'Device: ')
             if not choice.isdigit() or int(choice) < 0 or int(choice) >= len(devices):
-                print 'Invalid choice'
-                return interactive_config(option)
+                print Fore.RED + 'Invalid choice' + Style.RESET_ALL
+                return self.interactive_config(option)
             return devices[int(choice)]['index']
         if option == 'playback_device':
-            devices = [x for x in self.audio.get_devices() if x['maxOuputChannels'] > 0]
-            print "Please choose a device to monitor for transmissions:"
+            devices = [x for x in self.audio.get_devices() if x['maxOutputChannels'] > 0]
+            print Style.BRIGHT + "Please choose a device to use for transmitting:" + Style.RESET_ALL
             for x in range(len(devices)):
-                print "%i) %s" % (x, devices[x]['name'])
-            choice = raw_input('Device: ')
+                print (Style.BRIGHT + Fore.GREEN + "%i)" + Fore.BLUE + " %s" + Style.RESET_ALL) % (x, devices[x]['name'])
+            choice = raw_input(Style.BRIGHT + 'Device: ')
             if not choice.isdigit() or int(choice) < 0 or int(choice) >= len(devices):
-                print 'Invalid choice'
-                return interactive_config(option)
+                print Fore.RED + 'Invalid choice' + Style.RESET_ALL
+                return self.interactive_config(option)
             return devices[int(choice)]['index']
 
 if __name__ == '__main__':
